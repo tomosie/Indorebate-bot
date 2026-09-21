d384476be694db66ff63523b4fe27770810b6512
tomosie <tomosie@gmail.com>
2026-09-22 05:56:03 +0700

    Update report-daily.js

--- api/report-daily.js
+++ api/report-daily.js
@@ -12,6 +12,7 @@ const BROKERS = [
   { key: 'pindah_justmarkets', name: 'JustMarkets' },
   { key: 'pindah_roboforex', name: 'RoboForex' },
   { key: 'pindah_xm', name: 'XM Global' },
+  { key: 'pindah_tmgm', name: 'TMGM' },
 ];
 
 function wibDateString(offsetDays = 0) {
